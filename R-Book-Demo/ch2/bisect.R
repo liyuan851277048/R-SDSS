770a93eadd48aa7dbf43ab53cd82a1ac71654390
Eric Guo <eric.guo@sandisk.com>
2013-01-05 15:22:20 +0800

    Book example 2.4 in Page 133 in PDF, Page 99 in Paper version

---
+++ R-Book-Demo/ch2/bisect.R
@@ -0,0 +1,22 @@
+# Example 2.4 in Page 133 in PDF, Page 99 in Paper version
+
+fzero<-function(f, a, b, eps=1e-5){
+   if (f(a)*f(b)>0)
+      list(fail="finding root is fail!")
+   else{
+      repeat {
+         if (abs(b-a)<eps) break;
+         x<-(a+b)/2;
+         if (f(a)*f(x)<0) b<-x  else  a<-x
+      }
+      list(root=(a+b)/2, fun=f(x))
+   }
+}
+
+f<-function(x) x^3-x-1
+
+fzero(f,1,2)
+
+fzero(f,0,1)
+
+uniroot(f, c(1,2))

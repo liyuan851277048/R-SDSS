70dbceb298a17b9804a708a1924ea1e933258a37
Eric Guo <eric.guo@sandisk.com>
2013-02-04 15:55:16 +0800

    Martin provide the table

--- DB_mon/server.R
+++ DB_mon/server.R
@@ -1,24 +1,37 @@
 library(shiny)
 library(datasets)
+library("DBI")
+library("ROracle")
 
+drv <- dbDriver("Oracle")
+con <- dbConnect(drv, user  =  "system",  password="a",  db="srcdb")
+qDF <- dbSendQuery(con, "select file_id,file_name,bytes from dba_data_files")
+dDF <- fetch(qDF)
+qDept <- dbSendQuery(con, "select * from scott.dept")
+dDept <- fetch(qDept)
+qSalGrd <- dbSendQuery(con, "select * from scott.SALGRADE")
+dSalGrd <- fetch(qSalGrd)
 # Define server logic required to summarize and view the selected dataset
 shinyServer(function(input, output) {
   
   # Return the requested dataset
   datasetInput <- reactive(function() {
-    switch(input$db,
-           "TestDB" = rock,
-           "TestDB2" = pressure)
+    switch(input$dataset,
+           "Datafiles" = dDF,
+           "dept" = dDept,
+           "salgrd" = dSalGrd)
   })
   
   # Generate a summary of the dataset
-  output$summary <- reactivePrint(function() {
-    dataset <- datasetInput()
-    summary(dataset)
-  })
+  # output$summary <- reactivePrint(function() {
+  #   dataset <- datasetInput()
+  #   summary(dataset)
+  # })
   
   # Show the first "n" observations
   output$view <- reactiveTable(function() {
-    head(datasetInput(), n = 10)
+    #head(datasetInput())
+    datasetInput()
   })
+  
 })
--- DB_mon/ui.R
+++ DB_mon/ui.R
@@ -3,35 +3,20 @@ library(shiny)
 # Define UI for dataset viewer application
 shinyUI(pageWithSidebar(
   
-  # Application title.
-  headerPanel("DB Monitor"),
+  # Application title
+  headerPanel("Table Browser"),
   
   # Sidebar with controls to select a dataset and specify the number
-  # of observations to view. The helpText function is also used to 
-  # include clarifying text. Most notably, the inclusion of a 
-  # submitButton defers the rendering of output until the user 
-  # explicitly clicks the button (rather than doing it immediately
-  # when inputs change). This is useful if the computations required
-  # to render output are inordinately time-consuming.
+  # of observations to view
   sidebarPanel(
-    selectInput("db", "Choose a db:", 
-                choices = c("TestDB","TestDB2")),
-        
-    helpText("Note: while the data view will show only the specified",
-             "number of observations, the summary will still be based",
-             "on the full dataset."),
-    
-    submitButton("Update View")
+    selectInput("dataset", "Choose a Facet:", 
+                choices = c("Datafiles", "dept", "salgrd"))
   ),
   
   # Show a summary of the dataset and an HTML table with the requested
-  # number of observations. Note the use of the h4 function to provide
-  # an additional header above each output section.
+  # number of observations
   mainPanel(
-    h4("Summary"),
-    verbatimTextOutput("summary"),
     
-    h4("Observations"),
     tableOutput("view")
   )
 ))

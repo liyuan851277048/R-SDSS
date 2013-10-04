9271e7b19b4caf357a38b9d6e8937c78fd3e122d
Eric Guo <eric.guo@sandisk.com>
2013-10-04 15:07:03 +0800

    Comment out the case sensitive problem

--- SQL_R_Training/generate_data.R
+++ SQL_R_Training/generate_data.R
@@ -1,11 +1,14 @@
 library(ROracle)
-drv <- dbDriver("Oracle")
-con <- dbConnect(drv, username="sql_training", password="sq1trn", dbname="SPMSTST")
+con <- dbConnect(dbDriver("Oracle"), username="sql_training", password="sq1trn", dbname="SPMSTST")
+if (dbExistsTable(con, "DOG")) dbRemoveTable(con, "DOG")
+if (dbExistsTable(con, "OWNER")) dbRemoveTable(con, "OWNER")
 
-if (dbExistsTable(con, "DOGS")) dbRemoveTable(con, "DOGS")
-dogs <- data.frame(name=c('Fido','Bob','A Huang','Wolf'),owner_id=c(1,1,2,NA))
-dbWriteTable(con, "DOGS", dogs, row.names = FALSE)
+dogs <- data.frame(name=c('Fido','Bob','A Huang','Wolf'),owner_id=as.integer(c(1,1,2,NA)))
+dbWriteTable(con, "DOG", dogs, row.names = FALSE)
 
-if (dbExistsTable(con, "OWNERS")) dbRemoveTable(con, "OWNERS")
-owners <- data.frame(id=c(1,2,3),owner=c('Eric','Alick','Cindy'))
-dbWriteTable(con, "OWNERS", owners, row.names = FALSE)
+owners <- data.frame(id=as.integer(c(1,2,3)),owner=c('Eric','Alick','Cindy'))
+dbWriteTable(con, "OWNER", owners, row.names = FALSE)
+
+# due to ROracle dbWriteTable is case sensitive, 
+# so have to rebuild dog as dogs and owner as owners in Oracle
+# to avoid the case sensitive problem

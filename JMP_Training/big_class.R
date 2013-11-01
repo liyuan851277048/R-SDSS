3468845733a07adaa04c84e94eb4db32c7cbbef2
Eric Guo <eric.guo@sandisk.com>
2013-11-01 21:46:44 +0800

    Fix bug which weight start at line 2

--- JMP_Training/big_class.R
+++ JMP_Training/big_class.R
@@ -4,8 +4,8 @@ height <- read.xlsx("raw data_calss.xlsx","身高")
 height <- transform(height, name=姓名, age=as.integer(年龄), sex=substr(toupper(性别),1,1), height=as.integer(身高))
 height <- height[,c("name","age","sex","height")]
 
-weight <- read.xlsx("raw data_calss.xlsx","体重")
-weight <- transform(weight[-1,], girl=女生, boy=男生, girl_weight=NA., boy_weight=NA..1)
+weight <- read.xlsx("raw data_calss.xlsx","体重",startRow=2)
+weight <- transform(weight, girl=姓名, girl_weight=体重, boy=姓名.1, boy_weight=体重.1)
 
 gw <- weight[!is.na(weight$girl),]
 gw <- transform(gw, name=girl, weight=as.integer(girl_weight))
@@ -15,6 +15,6 @@ bw <- weight[!is.na(weight$boy),]
 bw <- transform(bw, name=boy, weight=as.integer(boy_weight))
 bw <- bw[,c("name","weight")]
 
-weight <- rbind(gw,bw)
+gbw <- rbind(gw,bw)
 
-bc <- merge(height, weight, by="name")
+bc <- merge(height, gbw, by="name")

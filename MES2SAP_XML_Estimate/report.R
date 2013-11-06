ae3688edb53cb815978c0739268cf11b88831496
Eric Guo <eric.guo@sandisk.com>
2013-11-06 15:13:38 +0800

    Get the possible SentNum via PNLevel

--- MES2SAP_XML_Estimate/report.R
+++ MES2SAP_XML_Estimate/report.R
@@ -7,13 +7,13 @@ intd_con <- dbConnect(dbDriver("Oracle"),
 fetchMESSentData <- function(intd_con) {
   sql_text = "
   select b.txndatetime, b.ordername, ptl.productlevelname PNLevel, b.processtype, b.product, b.txnname,
-         d.transactionid, d.saplocation, d.qty, d.rejectcode, d.specname Step, d.lotname,b.recid,d.labelid
+         d.transactionid, d.bincategory, d.saplocation, d.qty, d.rejectcode, d.specname Step, d.lotname,b.recid,d.labelid
   from sap_basic b
   LEFT JOIN sap_details d ON d.recid=b.recid
   LEFT JOIN productbase ptb ON ptb.productname=b.product
   LEFT JOIN product pt ON pt.productid=ptb.revofrcdid
   LEFT JOIN a_productlevel ptl ON ptl.productlevelid=pt.productlevelid
-  WHERE b.txndatetime > SYSDATE - 1
+  WHERE b.txndatetime > SYSDATE - 2
 "
   
   tb <- fetch(dbSendQuery(intd_con,sql_text), n= -1)
@@ -21,3 +21,6 @@ fetchMESSentData <- function(intd_con) {
 }
 
 pd <- fetchMESSentData(intd_con)
+u <- unique(subset(pd,TRUE, c(TXNNAME, PNLEVEL, PROCESSTYPE, TRANSACTIONID,BINCATEGORY,SAPLOCATION,LABELID)))
+u$SentNum <- ifelse(u$PROCESSTYPE == "Cherry Pick", 2, 1)
+d <- aggregate(SentNum ~ PNLEVEL, data = u, sum)

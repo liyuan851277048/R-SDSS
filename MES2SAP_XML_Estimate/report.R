a7dee07bcd7be8a3bdfd5cc3314bd8ca218ae78e
Eric Guo <eric.guo@sandisk.com>
2013-11-06 14:14:59 +0800

    initial version of MES2SAP XML Estimate report

---
+++ MES2SAP_XML_Estimate/report.R
@@ -0,0 +1,23 @@
+library(ROracle)
+intd_con <- dbConnect(dbDriver("Oracle"),
+                      username="insitetapintd",
+                      password="insitetapintd",
+                      dbname="MESWIP45.SDCORP.GLOBAL.SANDISK.COM")
+
+fetchMESSentData <- function(intd_con) {
+  sql_text = "
+  select b.txndatetime, b.ordername, ptl.productlevelname PNLevel, b.processtype, b.product, b.txnname,
+         d.transactionid, d.saplocation, d.qty, d.rejectcode, d.specname Step, d.lotname,b.recid,d.labelid
+  from sap_basic b
+  LEFT JOIN sap_details d ON d.recid=b.recid
+  LEFT JOIN productbase ptb ON ptb.productname=b.product
+  LEFT JOIN product pt ON pt.productid=ptb.revofrcdid
+  LEFT JOIN a_productlevel ptl ON ptl.productlevelid=pt.productlevelid
+  WHERE b.txndatetime > SYSDATE - 1
+"
+  
+  tb <- fetch(dbSendQuery(intd_con,sql_text), n= -1)
+  transform(tb, QTY=as.integer(as.character(QTY)))
+}
+
+pd <- fetchMESSentData(intd_con)

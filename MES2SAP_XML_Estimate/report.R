e628ece34dd610d70b0ce32db046d07cc279df29
Eric Guo <eric.guo@sandisk.com>
2013-11-07 05:57:57 +0800

    prepare report.R to used in shiny app

--- MES2SAP_XML_Estimate/report.R
+++ MES2SAP_XML_Estimate/report.R
@@ -4,8 +4,8 @@ intd_con <- dbConnect(dbDriver("Oracle"),
                       password="insitetapintd",
                       dbname="MESWIP45.SDCORP.GLOBAL.SANDISK.COM")
 
-fetchMESSentData <- function(intd_con) {
-  sql_text = "
+fetchMESSentData <- function(intd_con, day) {
+  sql_text = paste("
   select b.txndatetime, b.ordername, ptl.productlevelname PNLevel, b.processtype, b.product, b.txnname,
          d.transactionid, d.bincategory, d.saplocation, d.qty, d.rejectcode, d.specname Step, d.lotname,b.recid,d.labelid
   from sap_basic b
@@ -13,14 +13,21 @@ fetchMESSentData <- function(intd_con) {
   LEFT JOIN productbase ptb ON ptb.productname=b.product
   LEFT JOIN product pt ON pt.productid=ptb.revofrcdid
   LEFT JOIN a_productlevel ptl ON ptl.productlevelid=pt.productlevelid
-  WHERE b.txndatetime > SYSDATE - 2
-"
+  WHERE b.txndatetime > SYSDATE - ",day,sep="")
   
   tb <- fetch(dbSendQuery(intd_con,sql_text), n= -1)
   transform(tb, QTY=as.integer(as.character(QTY)))
 }
 
-pd <- fetchMESSentData(intd_con)
-u <- unique(subset(pd,TRUE, c(TXNNAME, PNLEVEL, PROCESSTYPE, TRANSACTIONID,BINCATEGORY,SAPLOCATION,LABELID)))
-u$SentNum <- ifelse(u$PROCESSTYPE == "Cherry Pick", 2, 1)
-d <- aggregate(SentNum ~ PNLEVEL, data = u, sum)
+
+preparePieData <- function(pd) {
+  u <- unique(subset(pd,TRUE, c(TXNNAME, PNLEVEL, PROCESSTYPE, TRANSACTIONID,BINCATEGORY,SAPLOCATION,LABELID)))
+  u$SentNum <- ifelse(u$PROCESSTYPE == "Cherry Pick", 2, 1)
+  d <- aggregate(SentNum ~ PNLEVEL, data = u, sum)
+  x <- as.vector(d$SentNum)
+  names(x) <- d$PNLEVEL
+  x  
+}
+
+pd <- fetchMESSentData(intd_con,1)
+x  <- preparePieData(pd)

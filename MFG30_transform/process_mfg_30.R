bafda368387a8f66816e3ee9ce4c07bc5e7e5f28
Eric Guo <eric.guo@sandisk.com>
2013-11-11 22:24:34 +0800

    initial version of process MFG30 report

---
+++ MFG30_transform/process_mfg_30.R
@@ -0,0 +1,5 @@
+options(java.parameters = "-Xmx800m")
+library(rJava)
+library(xlsx)
+mfg30_d <- read.xlsx2("MFG30.xlsx", sheetName="Detail", startRow=2, colClasses=c("integer",rep("character",3),"integer",rep("character",12),"integer","character",rep("integer",5),"character",rep("POSIXct",3),rep("character",6)))
+mfg30_d$X. <- NULL

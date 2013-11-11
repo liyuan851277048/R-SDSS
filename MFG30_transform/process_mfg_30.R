cb2da5697a9cea981f78ef615acb68ef44bf184f
Eric Guo <eric.guo@sandisk.com>
2013-11-11 22:42:36 +0800

    optimize the JVM memory usage

--- MFG30_transform/process_mfg_30.R
+++ MFG30_transform/process_mfg_30.R
@@ -1,4 +1,4 @@
-options(java.parameters = "-Xmx800m")
+options(java.parameters = "-Xmx512m")
 library(rJava)
 library(xlsx)
 mfg30_d <- read.xlsx2("MFG30.xlsx", sheetName="Detail", startRow=2, colClasses=c("integer",rep("character",3),"integer",rep("character",12),"integer","character",rep("integer",5),"character",rep("POSIXct",3),rep("character",4),"integer","character"))
